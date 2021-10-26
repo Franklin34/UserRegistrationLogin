36eb10dc375aa5e094c10c31b67cce0b7f60a276
Franklin34 <franklin.sandoval42@gmail.com>
2021-10-25 22:18:06 -0600

    Login ASP NEt

---
+++ Registro de Usuarios ASP NET/Registro de Usuarios ASP NET/Models/LoginModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Registro_de_Usuarios_ASP_NET.Models
+{
+    public class LoginModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }     
+    }
+}
